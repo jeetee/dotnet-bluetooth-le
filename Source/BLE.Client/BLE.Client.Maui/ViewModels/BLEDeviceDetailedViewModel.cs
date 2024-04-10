1207dd01fc91af3091d1aede691b4fea97513e21
jeetee <github@jeetee.net>
2024-04-10 23:42:14 +0200

    Fetching services and characteristic information

--- Source/BLE.Client/BLE.Client.Maui/ViewModels/BLEDeviceDetailedViewModel.cs
+++ Source/BLE.Client/BLE.Client.Maui/ViewModels/BLEDeviceDetailedViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Plugin.BLE;
+﻿using CommunityToolkit.Mvvm.Input;
+using Plugin.BLE;
 using Plugin.BLE.Abstractions;
 using Plugin.BLE.Abstractions.Contracts;
 using Plugin.BLE.Abstractions.EventArgs;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace BLE.Client.Maui.ViewModels;
@@ -16,6 +18,22 @@ public class BLEDeviceDetailedViewModel : BaseViewModel
     public String MacAddress { get; private init; }
     public IReadOnlyList<AdvertisementRecord> Adverts => _device.AdvertisementRecords;
 
+    public IReadOnlyList<IService> _Services = null;
+    public IReadOnlyList<IService> Services
+    {
+        get => _Services;
+        private set
+        {
+            _Services = value;
+            RaisePropertyChanged();
+            RaisePropertyChanged(nameof(ServiceCount));
+        }
+    }
+    public int ServiceCount => _Services?.Count ?? 0;
+
+    private Dictionary<IService, IReadOnlyList<ICharacteristic>> _Characteristics;
+    public ReadOnlyDictionary<IService, IReadOnlyList<ICharacteristic>> Characteristics { get; private set; } = null;
+
     public BLEDeviceDetailedViewModel(IDevice device)
     {
         _device = device;
@@ -26,20 +44,79 @@ public class BLEDeviceDetailedViewModel : BaseViewModel
         Adapter.DeviceConnectionLost += Adapter_DeviceConnectionLost;
         Adapter.DeviceDisconnected += Adapter_DeviceDisconnected;
 
+        DiscoverServicesCommand = new RelayCommand(DiscoverServices, CanDiscoverServices);
         DisconnectCommand = new Command(Disconnect);
     }
+
+    #region Services
+    public RelayCommand DiscoverServicesCommand { get; init; }
+    CancellationTokenSource _discoveryCancellationTokenSource = null;
+    private bool CanDiscoverServices()
+    {
+        return Connected && (_discoveryCancellationTokenSource == null);
+    }
+    private async void DiscoverServices()
+    {
+        if (_discoveryCancellationTokenSource == null)
+        {
+            App.Logger.AddMessage("Starting discovery of services");
+            _discoveryCancellationTokenSource = new();
+            DiscoverServicesCommand.NotifyCanExecuteChanged();
+            IReadOnlyList<IService> services = null;
+            try
+            {
+                services = await _device.GetServicesAsync(_discoveryCancellationTokenSource.Token);
+                App.Logger.AddMessage($"Discovered {services.Count} services");
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Services = services;
+                });
+
+                _Characteristics = [];
+                foreach(var service in Services)
+                {
+                    App.Logger.AddMessage($"Retrieving characteristics for service {service.Id}");
+                    _Characteristics[service] = await service.GetCharacteristicsAsync();
+                    App.Logger.AddMessage($"Received {_Characteristics[service].Count} characteristics");
+                }
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Characteristics = new(_Characteristics);
+                    RaisePropertyChanged(nameof(Characteristics));
+                });
+            }
+            catch (Exception e)
+            {
+                App.Logger.AddMessage($"Discovery failed: {e}");
+            }
+            _discoveryCancellationTokenSource.Dispose();
+            _discoveryCancellationTokenSource = null;
+            DiscoverServicesCommand.NotifyCanExecuteChanged();
+            App.Logger.AddMessage("Discovery... DONE");
+        }
+    }
+    #endregion Services
 
     #region Disconnect
     private void Adapter_DeviceConnectionLost(object sender, DeviceErrorEventArgs e)
     {
         App.Logger.AddMessage($"DeviceConnectionLost:\n{e.Device.Name}\n{e.Device.Id}\n{e.ErrorMessage}");
-        RaisePropertyChanged(nameof(Connected));
+        HandleDisconnectedDevice(e.Device);
     }
 
     private void Adapter_DeviceDisconnected(object sender, DeviceEventArgs e)
     {
         App.Logger.AddMessage($"DeviceDisconnected:\n{e.Device.Name}\n{e.Device.Id}");
-        RaisePropertyChanged(nameof(Connected));
+        HandleDisconnectedDevice(e.Device);
+    }
+
+    private void HandleDisconnectedDevice(IDevice _)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            RaisePropertyChanged(nameof(Connected));
+            DiscoverServicesCommand.NotifyCanExecuteChanged();
+        });
     }
 
     public ICommand DisconnectCommand { get; init; }
