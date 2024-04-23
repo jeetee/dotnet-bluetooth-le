f6c677fb05ac94845e6c4cc661b07683826734f5
jeetee <github@jeetee.net>
2024-04-23 10:56:05 +0200

    Characteristic Subscription

--- Source/BLE.Client/BLE.Client.Maui/Views/DeviceCharacteristics.xaml.cs
+++ Source/BLE.Client/BLE.Client.Maui/Views/DeviceCharacteristics.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace BLE.Client.Maui.Views;
+﻿using Plugin.BLE.Abstractions.Contracts;
+
+namespace BLE.Client.Maui.Views;
 
 public partial class DeviceCharacteristics : ContentPage
 {
@@ -6,4 +8,38 @@ public partial class DeviceCharacteristics : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void SubscribeToCharacteristicUpdates_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+		if (sender is not null)
+		{
+			ICharacteristic characteristic = (ICharacteristic)((CheckBox)sender).BindingContext;
+			if (e.Value == true)
+			{
+				try
+				{
+					await characteristic.StartUpdatesAsync();
+                    App.Logger.AddMessage($"Subscribed to {characteristic.Id}");
+                }
+				catch (Exception ex)
+				{
+					App.Logger.AddMessage($"Failed to Subscribe to {characteristic.Id}\n{ex}");
+					App.AlertSvc.ShowAlert("Failed to Subscribe", ex.ToString());
+				}
+			}
+			else
+			{
+                try
+                {
+                    await characteristic.StopUpdatesAsync();
+                    App.Logger.AddMessage($"Unsubscribed from {characteristic.Id}");
+                }
+                catch (Exception ex)
+                {
+                    App.Logger.AddMessage($"Failed to Unsubscribe from {characteristic.Id}\n{ex}");
+                    App.AlertSvc.ShowAlert("Failed to Unsubscribe", ex.ToString());
+                }
+            }
+		}
+    }
 }
